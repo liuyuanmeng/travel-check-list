8d4362ef84f95d3ae1a5033689453697ce5fdba9
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2024-01-09 10:06:01 +0000

    building a form and handling submissions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "hat", quantity: 1, packed: true },
+]
 
-function App() {
+export default function App() {
+  return <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
+
+
+}
+function Logo() {
+  return <h1>🏖 Far Away Holiday</h1>
+
+}
+function Form() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form className="add-form">
+      <h3>What do you need for your trip?</h3>
+      <select>
+        {/* javascript array form, use alot in react */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}></option>
+        ))}
+      </select>
+
+    </form>
+  )
+
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul >
+        {/* the name of the componet,the name of the prop and then here the object itsef call back function */}
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+
+
+      </ul>
     </div>
   );
+
+}
+function Item({ item }) {
+  return <li><span style={item.packed ? { textDecoration: "line-through" } : {}}>{item.description} {item.quantity}</span>
+    <button>❌</button></li>
+
 }
 
-export default App;
+function Stats() {
+  return <footer className="stats">
+    <em>you have x items on your list, and you already packed x (x%)</em>
+  </footer>
+
+}

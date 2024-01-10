b0fa61f297773ecd47907e6d0c34462d09339610
Yuanmeng Liu <liuyuanmeng66@gmail.com>
2024-01-10 14:59:08 +0000

    add item state handlers

--- src/App.js
+++ src/App.js
@@ -1,52 +1,68 @@
-import { useState } from "react"
-
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "hat", quantity: 1, packed: true },
-]
+import { useState } from "react";
 
 export default function App() {
-  return <div className="app">
-    <Logo />
-    <Form />
-    <PackingList />
-    <Stats />
-  </div>
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    // new state depends on the current state, callback function, create a new array contains all the current items plus the new one
+    setItems((items) => [...items, item]);
+  }
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  // update an object in an array
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item,
+      ),
+    );
+  }
 
-
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats />
+    </div>
+  );
 }
 function Logo() {
-  return <h1>🏖 Far Away Holiday</h1>
-
+  return <h1>🏖 Far Away Holiday</h1>;
 }
-function Form() {
+function Form({ onAddItems }) {
   // controlled element -> create a piece of state
   // use on the element that we want to control
   // update the state
-  const [description, setDescription] = useState("")
-  const [quantity, setQuantity] = useState(1)
-
-
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  // initial  state for the items is just an empty array
   // avid reload
   function handleSubmit(e) {
-    e.preventDefault()
-    console.log(e)
+    e.preventDefault();
+    console.log(e);
+    // add a guard clause
+    if (!description) return;
 
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    // once submited go back to original
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
-  // add a guard clause
-  if (!description) return
-
-  const newItem = { description, quantity, packed: false, id: Date.now() }
-  // once submited go back to original 
-  setDescription('')
-  setQuantity(1)
-
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
       {/* the value is walys string needed coverted */}
-      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {/* javascript array form, use alot in react */}
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
@@ -54,38 +70,58 @@ function Form() {
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."
+      <input
+        type="text"
+        placeholder="Item..."
         // always need the value and the onchange
-        value={description} onChange={(e) => setDescription(e.target.value)} />
-
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
     </form>
-  )
-
+  );
 }
-function PackingList() {
+
+function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
-      <ul >
+      <ul>
         {/* the name of the componet,the name of the prop and then here the object itsef call back function */}
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onDeleteItem}
+            key={item.id}
+          />
         ))}
-
-
       </ul>
     </div>
   );
-
 }
-function Item({ item }) {
-  return <li><span style={item.packed ? { textDecoration: "line-through" } : {}}>{item.description} {item.quantity}</span>
-    <button>❌</button></li>
-
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+    // call the delite only whenn the event happends,
+  );
 }
 
 function Stats() {
-  return <footer className="stats">
-    <em>you have x items on your list, and you already packed x (x%)</em>
-  </footer>
-
+  return (
+    <footer className="stats">
+      <em>you have x items on your list, and you already packed x (x%)</em>
+    </footer>
+  );
 }
+
+
